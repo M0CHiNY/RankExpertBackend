9e868e4945f7d2ab4b3e3991088e8491faf0e370
Yurii Khrystiuk <108680301+M0CHiNY@users.noreply.github.com>
2023-12-16 14:49:49 +0200

    add logos

--- web/wp-content/themes/rankexperts/functions.php
+++ web/wp-content/themes/rankexperts/functions.php
@@ -7,9 +7,9 @@
  * @package rankexperts
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,43 +19,44 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function rankexperts_setup() {
+function rankexperts_setup()
+{
 	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on rankexperts, use a find and replace
-		* to change 'rankexperts' to the name of your theme in all the template files.
-		*/
-	load_theme_textdomain( 'rankexperts', get_template_directory() . '/languages' );
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on rankexperts, use a find and replace
+	 * to change 'rankexperts' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain('rankexperts', get_template_directory() . '/languages');
 
 	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
-	add_theme_support( 'title-tag' );
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support('title-tag');
 
 	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
-	add_theme_support( 'post-thumbnails' );
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'header-menu' => esc_html__( 'Primary', 'rankexperts' ),
-			'footer-menu-left' => esc_html__( 'Footer menu left', 'rankexperts' ),
-			'footer-menu-right' => esc_html__( 'Footer menu right', 'rankexperts' ),
+			'header-menu' => esc_html__('Primary', 'rankexperts'),
+			'footer-menu-left' => esc_html__('Footer menu left', 'rankexperts'),
+			'footer-menu-right' => esc_html__('Footer menu right', 'rankexperts'),
 		)
 	);
 
 	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
 	add_theme_support(
 		'html5',
 		array(
@@ -70,24 +71,10 @@ function rankexperts_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
 }
-add_action( 'after_setup_theme', 'rankexperts_setup' );
+add_action('after_setup_theme', 'rankexperts_setup');
 
 
 /**
@@ -107,3 +94,8 @@ require get_template_directory() . '/inc/headerWalkerMenu.php';
  * Implement footer menu
  */
 require get_template_directory() . '/inc/footerWalkerMenu.php';
+
+/**
+ * Implement customizer logos
+ */
+require get_template_directory() . '/inc/customizer-logos.php';
--- web/wp-content/themes/rankexperts/header.php
+++ web/wp-content/themes/rankexperts/header.php
@@ -29,10 +29,18 @@
         <div class="header__inner">
 
           <!-- logo start -->
+          <?php $header_logo = get_theme_mod('header_logo');
+          $header_attachment_id = attachment_url_to_postid($header_logo);
+          $header_logo_alt = get_post_meta($header_attachment_id, '_wp_attachment_image_alt', true);
+          ?>
+
           <div class="logo-box">
-            <a href="#" class="logo-box__link">
-              <img class="logo-box__img" src="images/logo-header.png" alt="">
-            </a>
+            <?php if ($header_logo): ?>
+              <a href="<?php echo home_url(); ?>" class="logo-box__link">
+                <img class="logo-box__img" src="<?php echo esc_url($header_logo); ?>"
+                  alt="<?php echo esc_attr($header_logo_alt) ?>">
+              </a>
+            <?php endif; ?>
           </div>
           <!-- logo end -->
 
@@ -45,22 +53,24 @@
 
                   <!-- logo start mobile-->
                   <div class="logo-box logo-box--mobile">
-                    <a href="#" class="logo-box__link">
-                      <img class="logo-box__img" src="../images/logo-header.png" alt="">
+                    <a href="<?php echo home_url(); ?>" class="logo-box__link">
+                      <img class="logo-box__img" src="<?php echo esc_url($header_logo); ?>"
+                        alt="<?php echo esc_attr($header_logo_alt) ?>">
                     </a>
                   </div>
                   <!-- logo end mobile-->
 
                   <button type="button" aria-label="Close" class="mob-nav-close" data-ui-dismiss>
                     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
-                      <path d="M35.1562 16L14.8438 36.3125M14.8438 16L35.1562 36.3125" stroke="#201F1D" stroke-width="4.6875" stroke-linecap="round" stroke-linejoin="round" />
+                      <path d="M35.1562 16L14.8438 36.3125M14.8438 16L35.1562 36.3125" stroke="#201F1D"
+                        stroke-width="4.6875" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                   </button>
                 </div>
                 <nav class="nav">
                   <?php
                   // display header meny
-
+                  
                   wp_nav_menu(array(
                     'theme_location' => 'header-menu',
                     'menu_class' => 'nav-menu',
@@ -90,7 +100,9 @@
             <!-- mobile btn start-->
             <button type="button" aria-label="Navigation" class="mob-nav-toggler" data-ui-toggle="mob-nav">
               <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8.33333 37.5H41.6667C42.8125 37.5 43.75 36.5625 43.75 35.4167C43.75 34.2708 42.8125 33.3333 41.6667 33.3333H8.33333C7.1875 33.3333 6.25 34.2708 6.25 35.4167C6.25 36.5625 7.1875 37.5 8.33333 37.5ZM8.33333 27.0833H41.6667C42.8125 27.0833 43.75 26.1458 43.75 25C43.75 23.8542 42.8125 22.9167 41.6667 22.9167H8.33333C7.1875 22.9167 6.25 23.8542 6.25 25C6.25 26.1458 7.1875 27.0833 8.33333 27.0833ZM6.25 14.5833C6.25 15.7292 7.1875 16.6667 8.33333 16.6667H41.6667C42.8125 16.6667 43.75 15.7292 43.75 14.5833C43.75 13.4375 42.8125 12.5 41.6667 12.5H8.33333C7.1875 12.5 6.25 13.4375 6.25 14.5833Z" fill="#201F1D" />
+                <path
+                  d="M8.33333 37.5H41.6667C42.8125 37.5 43.75 36.5625 43.75 35.4167C43.75 34.2708 42.8125 33.3333 41.6667 33.3333H8.33333C7.1875 33.3333 6.25 34.2708 6.25 35.4167C6.25 36.5625 7.1875 37.5 8.33333 37.5ZM8.33333 27.0833H41.6667C42.8125 27.0833 43.75 26.1458 43.75 25C43.75 23.8542 42.8125 22.9167 41.6667 22.9167H8.33333C7.1875 22.9167 6.25 23.8542 6.25 25C6.25 26.1458 7.1875 27.0833 8.33333 27.0833ZM6.25 14.5833C6.25 15.7292 7.1875 16.6667 8.33333 16.6667H41.6667C42.8125 16.6667 43.75 15.7292 43.75 14.5833C43.75 13.4375 42.8125 12.5 41.6667 12.5H8.33333C7.1875 12.5 6.25 13.4375 6.25 14.5833Z"
+                  fill="#201F1D" />
               </svg>
             </button>
             <!-- mobile btn end-->
