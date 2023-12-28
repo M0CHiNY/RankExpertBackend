78b44799b5d189ee19d6d37e41e59d05af3a641d
Yurii Khrystiuk <108680301+M0CHiNY@users.noreply.github.com>
2023-12-28 14:13:41 +0200

    all services

---
+++ web/wp-content/themes/rankexperts/template-services.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Template name: All services
+ */
+
+get_header();
+?>
+
+<!-- section with all services start -->
+<?php get_template_part('template-parts/all-services/all-services'); ?>
+<!-- section with all services end -->
+
+<?php
+get_footer();
